3de34336a312a62edf2f33a7a28e7f6231c1b310
Gonzalo Simon <53063584+gb-simon@users.noreply.github.com>
2019-12-15 04:35:41 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,185 +1,50 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const path = require('path');
-require('dotenv').config();
-const User = require('./models/user');
-const Exercise = require('./models/exercise');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
 
-const app = express();
-const port = process.env.PORT;
-const mongoDB = process.env.MLAB_URI;
+const cors = require('cors')
 
-mongoose.connect(mongoDB);
-const db = mongoose.connection;
-db.on('error', (err) => { console.log('Mongo DB connection error', err); });
-db.once('open', () => { console.log('Mongo DB connected.'); });
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
 
-app.use(cors({ optionSuccessStatus: 200 }));  // some legacy browsers choke on 204
+app.use(cors())
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
-app.use(express.static('public'));
 
+app.use(express.static('public'))
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'));
+  res.sendFile(__dirname + '/views/index.html')
 });
 
-// Create new User route
-app.post('/api/exercise/new-user/', (req, res) => {
-  const username = req.body.username;
+const apiRouter = require('./routes/api')
+app.use('/api/exercise', apiRouter)
 
-  if (username === '') {
-    res.send('Username cannot be blank');
-  } else if (username.length > 10) {
-    res.send('Username cannot be greater than 10 characters');
+// Not found middleware
+app.use((req, res, next) => {
+  return next({status: 404, message: 'not found'})
+})
+
+// Error Handling middleware
+app.use((err, req, res, next) => {
+  let errCode, errMessage
+
+  if (err.errors) {
+    // mongoose validation error
+    errCode = 400 // bad request
+    const keys = Object.keys(err.errors)
+    // report the first validation error
+    errMessage = err.errors[keys[0]].message
   } else {
-    const newUser = new User({
-      username,
-    });
-
-    newUser.save((err, data) => {
-      if (err) {
-        if (err.name === 'MongoError' && err.code === 11000) { // Duplicate key error
-          res.send('Duplicate username, try a different username');
-        } else {
-          res.send('Errpr pccurred while saving user');
-        }
-      } else {
-        res.json(data);
-      }
-    });
+    // generic or custom error
+    errCode = err.status || 500
+    errMessage = err.message || 'Internal Server Error'
   }
-});
+  res.status(errCode).type('txt')
+    .send(errMessage)
+})
 
-// Create new Exercise route
-app.post('/api/exercise/add', (req, res) => {
-  const username = req.body.username;
-  const description = req.body.description;
-  let duration = req.body.duration;
-  let date = req.body.date;
-  let userId;
-
-  if (username === undefined || description === undefined || duration === undefined) {
-    res.send('Required Field(s) are missing.');
-  } else if (username === '' || description === '' || duration === '') {
-    res.send('Required Field(s) are blank.');
-  } else if (username.length > 10) {
-    res.send('Username cannot be greater than 10 characters');
-  } else if (description.length > 100) {
-    res.send('Description cannot be greater than 100 characters');
-  } else if (isNaN(duration)) {
-    res.send('Duration must be a number');
-  } else if (Number(duration) > 1440) {
-    res.send('Duration must be less than 1440 minutes (24 hours)');
-  } else if (date !== '' && isNaN(Date.parse(date)) === true) {
-    res.send('Date is not a valid date');
-  } else {
-    // Find userId for username
-    User.findOne({ username }, (err, user) => {
-      if (err) {
-        res.send('Error while searching for username, try again');
-      } else if (!user) {
-        res.send('Username not found');
-      } else {
-        userId = user.id;
-
-        // Valiidations passed, convert duration
-        duration = Number(duration);
-
-        // Valiidations passed, convert date
-        if (date === '') {
-          date = new Date();
-        } else {
-          date = Date.parse(date);
-        }
-
-        const newExercise = new Exercise({
-          userId,
-          description,
-          duration,
-          date,
-        });
-
-        newExercise.save((errSave, data) => {
-          if (errSave) {
-            res.send('Error occurred while saving exercise');
-          } else {
-            res.json(data);
-          }
-        });
-      }
-    });
-  }
-});
-
-// Read Exercise route
-app.get('/api/exercise/:log', (req, res) => {
-  const username = req.query.username;
-  let from = req.query.from;
-  let to = req.query.to;
-  let limit = req.query.limit;
-  let userId;
-  const query = {};
-
-  // console.log(req.query);
-
-  if (username === undefined) {
-    res.send('Required Field(s) are missing.');
-  } else if (username === '') {
-    res.send('Required Field(s) are blank.');
-  } else if (username.length > 10) {
-    res.send('Username cannot be greater than 10 characters');
-  } else if (from !== undefined && isNaN(Date.parse(from)) === true) {
-    res.send('From is not a valid date');
-  } else if (to !== undefined && isNaN(Date.parse(to)) === true) {
-    res.send('From is not a valid date');
-  } else if (limit !== undefined && isNaN(limit) === true) {
-    res.send('Limit is not a valid number');
-  } else if (limit !== undefined && Number(limit) < 1) {
-    res.send('Limit must be greater than 0');
-  } else {
-    // Find userId for username\
-    User.findOne({ username }, (err, user) => {
-      if (err) {
-        res.send('Error while searching for username, try again');
-      } else if (!user) {
-        res.send('Username not found');
-      } else {
-        userId = user.id;
-        query.userId = userId;
-
-        if (from !== undefined) {
-          from = new Date(from);
-          query.date = { $gte: from};
-        }
-
-        if (to !== undefined) {
-          to = new Date(to);
-          to.setDate(to.getDate() + 1); // Add 1 day to include date
-          query.date = { $lt: to};
-        }
-
-        if (limit !== undefined) {
-          limit = Number(limit);
-        }
-
-        Exercise.find(query).select('userId description date duration ').limit(limit).exec((errExercise, exercises) => {
-          if (err) {
-            res.send('Error while searching for exercises, try again');
-          } else if (!user) {
-            res.send('Exercises not found');
-          } else {
-            res.json(exercises);
-          }
-        });
-      }
-    });
-  }
-});
-
-const listener = app.listen(port, () => {
-  console.log(`Your app is listening on port ${listener.address().port}`);
-});
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
